ab117fb5926971a8de1ecd3600c7348c7ec87819
David Torres <detour27@gmail.com>
2023-04-29 23:09:10 -0400

    add DB_PORT

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = {
     DB_USERNAME: process.env.DB_USERNAME,
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
     DB_DIALECT: process.env.DB_DIALECT,
     DB_STORAGE: process.env.DB_STORAGE,
 };
